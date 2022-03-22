4ce1a43cf3a4bdc0c3762771c30b360357e3747c
olivergrt <81304271+olivergrt@users.noreply.github.com>
2022-03-22 13:35:44 +0100

    Update choix_categorie.php

--- commande/choix_categorie.php
+++ commande/choix_categorie.php
@@ -13,7 +13,7 @@ $reqAllCateg = $bdd->query("SELECT id_categorie,libelle_categorie,img_categorie 
 			        	<td>
 			         		<h3 class="title-dark"><p class="text-primary"><?= $c['libelle_categorie'] ?></p></h3>
 			        		<a href="commander-categorie-<?= $c['id_categorie'] ?>">
-			        			<img class="imgpdt" style="width: 200px; height: 200px;" src="<?= $c['img_categorie'] ?>"><br><br>
+			        			<img class="imgpdt rounded" style="width: 200px; height: 200px;" src="<?= $c['img_categorie'] ?>"><br><br>
 			        		</a>
 			        		<a class="btn btn-outline-primary btn-lg" href="commander-categorie-<?= $c['id_categorie'] ?>">Choisir</a><br><br>
 			        	</td>
